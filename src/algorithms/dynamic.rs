996a16b25e827ec49d59301d99ee55239c0aaa40
SeraphyBR <luisjuniorbr@gmail.com>
2020-08-18 10:48:29 -0300

    Update

--- src/algorithms/dynamic.rs
+++ src/algorithms/dynamic.rs
@@ -7,16 +7,17 @@ use crate::point::Point;
 use crate::travel_route::TravelRoute;
 use super::algorithm::{Algorithm, TSPResult};
 
+use graph::Graph;
+
 type City = Point<i32>;
 type Route = Vec<usize>;
 type Cities = Vec<City>;
-type Graph = graph::Graph<f32>;
 type Population = Vec<TravelRoute>;
 type Matrix<T> = Array2<T>;
 
 pub struct Dynamic {
-    graph: Graph,
-    path: Matrix<i32>,
+    graph: Graph<f32>,
+    path: Matrix<Option<usize>>,
     dp: Matrix<f32>,
     min_path: f32,
     size: usize,
@@ -25,8 +26,31 @@ pub struct Dynamic {
 }
 
 impl Dynamic {
-    fn tsd(&mut self, mask: i32, source: i32) -> f32 {
-        todo!();
+    fn tsd(&mut self, mask: usize, source: usize) -> f32 {
+        if mask == self.visited {
+            return self.graph.get_connection(source, self.pos);
+        }
+        else if self.dp[(mask, source)] != -1.0 {
+            return self.dp[(mask, source)];
+        }
+
+        let mut ans = f32::MAX;
+        let mut min_ans = 0.0;
+        let mut k = None;
+        for i in 0..self.size {
+            if mask & (1 << i) == 0 {
+                min_ans = self.graph.get_connection(source, i) + self.tsd(mask | (1 << i), i);
+
+                if ans > min_ans {
+                    ans = min_ans;
+                    k = Some(i);
+                }
+            }
+        }
+
+        self.path[(mask, source)] = k;
+        self.dp[(mask, source)] = ans;
+        self.dp[(mask, source)]
     }
 }
 
@@ -40,7 +64,7 @@ impl Algorithm for Dynamic {
 
         Self {
             graph,
-            path: Matrix::from_shape_fn((size, size), |(_,_)| -1),
+            path: Matrix::default((size, size)),
             dp: Matrix::from_shape_fn((size, size), |(_,_)| -1.0),
             min_path: 0.0,
             size,
